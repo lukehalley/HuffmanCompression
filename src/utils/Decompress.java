6c5acd7d145b95d17d553c915df2715801f3fd91
Luke Halley <luke123halley@gmail.com>
2017-01-27 10:18:21 +1000

    Handle edge case for single character input

--- src/utils/Decompress.java
+++ src/utils/Decompress.java
@@ -4,3 +4,4 @@ import java.io.File;
 import java.util.Scanner;
 
 // Traverse tree using bit sequence to reconstruct original data
+// Handle special case when input contains only one unique character
