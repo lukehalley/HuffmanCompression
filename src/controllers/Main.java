2a6cb5a6b1fcef432ee4d33a43028e43555095e0
Luke Halley <luke123halley@gmail.com>
2017-10-11 13:40:21 +1000

    Add error handling documentation

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -27,6 +27,7 @@ public class Main {
 	public static void main(String args[]) throws Exception {
 		HashMap<Character, String> codeMap = new HashMap<Character, String>();
 
+// Handle file I/O errors and invalid input gracefully
 		Counter c = new Counter();
 
 		Counter.ImportString();
